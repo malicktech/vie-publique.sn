a296b8cb915ca042c0ecdb49b73e964eff285c52
Ndene SENE <ndenenafi10@gmail.com>
2025-02-18 23:12:16 +0100

    fix(PWA-SW): fix service worker and add images and api cache strategies

--- service-worker/sw.ts
+++ service-worker/sw.ts
@@ -13,7 +13,7 @@ declare let self: ServiceWorkerGlobalScope;
 // self.__WB_MANIFEST is default injection point
 const entries = self.__WB_MANIFEST
 if (import.meta.env.DEV)
-  entries.push({ url: '/', revision: Math.random().toString() })
+  entries.push({ url: '/', revision: Date.now().toString() });
 
 precacheAndRoute(entries)
 
@@ -37,6 +37,50 @@ if (import.meta.env.PROD) {
         new ExpirationPlugin({ maxEntries: 100 }),
       ],
     }),
+  )
+
+  // Cache des API avec stratégie "Network First"
+  registerRoute(
+    ({ url }) => url.pathname.startsWith('/items/'),
+    new NetworkFirst({
+      cacheName: 'api-cache',
+      plugins: [
+        new CacheableResponsePlugin({ statuses: [200] }),
+        new ExpirationPlugin({ maxEntries: 100, maxAgeSeconds: 3600 }), // 1h
+      ],
+    })
+  )
+
+  // Cache des images locales
+  registerRoute(
+    ({ request }) => request.destination === 'image',
+    new CacheFirst({
+      cacheName: 'images-cache',
+      plugins: [
+        new CacheableResponsePlugin({ statuses: [200] }), // Seules les réponses 200
+        new ExpirationPlugin({
+          maxEntries: 100, // Limite de stockage
+          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 jours
+        }),
+      ],
+    })
+  )
+
+  // Cache des images du CMS
+  registerRoute(
+    ({ url }) => 
+      url.origin === 'https://cms.vie-publique.sn' && 
+      url.pathname.startsWith('/assets'),
+    new StaleWhileRevalidate({
+      cacheName: 'cms-assets-images',
+      plugins: [
+        new CacheableResponsePlugin({ statuses: [200] }), // Uniquement les réponses 200 OK
+        new ExpirationPlugin({
+          maxEntries: 200, // Limite le nombre d'images stockées
+          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 jours de cache
+        }),
+      ],
+    })
   )
 }
 registerRoute(new NavigationRoute(
